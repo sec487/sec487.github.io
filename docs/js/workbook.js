75e4342ca62b81ac4f05c42d4760785044efa5e3
WebBreacher <WebBreacher@users.noreply.github.com>
2021-01-15 09:05:11 -0500

    fixing site

--- docs/js/workbook.js
+++
@@ -1,7 +0,0 @@
-// pulled from https://github.com/squidfunk/mkdocs-material/issues/791#issuecomment-396563541
-// modified by Phil Hagen
-
-// attach fancybox to all class="zoom" elements
-$(document).ready(function() {
-    $(".zoom").fancybox();
-});
